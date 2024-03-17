6c7a472bb1d2c037734cff4bef74e90e25a28f6a
eya ouertani <ouertaniaya50@gmail.com>
2024-03-17 23:45:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//imports 
+const express = require('express');
+//rest object 
+const app = express();
+
+
+//routes 
+app.get('/', (req, res) => {
+    res.send("<h1>welcome to meet rooms </h1>");
+})
+//listen 
+app.listen(8000, () => {
+    console.log(" node server running on 8000");
+})
