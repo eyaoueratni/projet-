f65e2773b5b91bf80b319cd54aaa0e6d9f7a3128
eya ouertani <ouertaniaya50@gmail.com>
2024-03-18 21:41:14 +0100

    securing node

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //imports 
 const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+// dor env config
+dotenv.config({ path: '' });
 //rest object 
 const app = express();
 
@@ -8,7 +12,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("<h1>welcome to meet rooms </h1>");
 })
+//port 
+
+const PORT = process.env.PORT || 9010;
 //listen 
-app.listen(8000, () => {
-    console.log(" node server running on 8000");
+app.listen(PORT, () => {
+    console.log(` node server running in ${process.env.DEV_MODE} MODE on PORT no ${PORT} `.bgCyan.white);
 })
