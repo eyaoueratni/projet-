fe2e6716bebdcd8b180f5324e37e52642c3a3c8c
eya ouertani <ouertaniaya50@gmail.com>
2024-03-20 22:14:49 +0100

    error middleware

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const cors = require('cors');
 const morgan = require('morgan');
+const expresserror = require('express-async-errors');
 //imports files
 const connectDB = require('./config/db.js');
 //routes imports
 const testRoutes = require('./controller/testController.js');
 const authRoutes = require('./routes/authRoutes.js');
+const errorMiddleware = require('./middlewares/errorMiddleware.js');
 // dot env config
 dotenv.config();
 //rest object 
@@ -22,7 +24,9 @@ app.use(cors());
 app.use(morgan("dev"));
 //routes 
 app.use('/test', testRoutes);
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+//validation middleware 
+app.use(errorMiddleware);
 //port 
 
 const PORT = process.env.PORT || 9010;
--- controller/authController.js
+++ controller/authController.js
@@ -1,38 +1,28 @@
 const userModel = require("../models/userModel.js");
 
-const registerController = async (req, res) => {
-    try {
-        const { name, email, password } = req.body
-        //validate
-        if (!name) {
-            return res.status(400).send({ success: false, message: 'please provide name' })
-        }
-        if (!email) {
-            return res.status(400).send({ success: false, message: 'please provide email' })
-        }
-        if (!password) {
-            return res.status(400).send({ success: false, message: 'please provide password' })
-        }
-        const existingUser = await userModel.findOne({ email })
-        if (existingUser) {
-            return res.status(200).send({
-                success: false,
-                message: 'email already exist please login'
-            });
-        }
-        const user = await userModel.create({ name, email, password });
-        res.status(201).send({
-            sucess: true,
-            message: 'User created successfully',
-            user,
-        });
-    } catch (error) {
-        console.log(error)
-        res.status(400).send({
-            message: 'error in register Controller',
-            success: false,
-            error
-        })
+const registerController = async (req, res, next) => {
+
+    const { name, email, password } = req.body
+    //validate
+    if (!name) {
+        next('name is required !');
     }
+    if (!email) {
+        next('email is required !');
+    }
+    if (!password) {
+        next('password is required !');
+    }
+    /*const existingUser = await userModel.findOne({ email })
+    if (existingUser) {
+        next('email already exist ,please login');
+    };*/
+    const user = await userModel.create({ name, email, password });
+    res.status(201).send({
+        sucess: true,
+        message: 'User created successfully',
+        user,
+    });
+
 };
 module.exports = registerController
