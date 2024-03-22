3c7b7ed3b4b3adb8ba2de10635c078ec655cb707
eya ouertani <ouertaniaya50@gmail.com>
2024-03-22 12:26:44 +0100

    authentificate

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const testController = require('../controller/testController.js')
+const testController = require('../controller/testController.js');
+const authenticate = require('../middlewares/authenticate.js');
 //router Object
 const router = express.Router();
 //routes
-router.post('/test-post', testContoller)
+router.post('/test-post', authenticate, testContoller)
 //export
 module.exports = testController;
